fbb15c2899b29bf27583a3c6386b647e55826160
Amalraj-Joseph-IBM <amalrajjoseph@ibm.com>
2025-02-18 18:04:19 +0530

    Introducing identical unit tests in multiple modules

---
+++ war/src/test/java/io/openliberty/guides/multimodules/UnitTest02.java
@@ -0,0 +1,18 @@
+package io.openliberty.guides.multimodules;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class UnitTest02 {
+
+    @Test
+    public void alwaysPass() {
+    }
+
+    @Ignore
+    @Test
+    public void alwaysSkip() {
+    }
+}
